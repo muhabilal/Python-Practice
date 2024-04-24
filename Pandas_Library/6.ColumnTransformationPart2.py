7ed75c99c69fa2922f9155e6cabb1cfb9b7dc5ef
M Bilal <bilaldarmuhammad@gmail.com>
2024-04-24 18:25:49 +0500

    column transformation part 2

--- Pandas_Library/6.ColumnTransformationPart2.py
+++ Pandas_Library/6.ColumnTransformationPart2.py
@@ -1,1 +1,10 @@
 import pandas as pd
+data={"Months":["January","Feburary","March","April"]}
+a=pd.DataFrame(data)
+print(a)
+
+def extract(value):
+    return value[0:3]
+
+a["short_months"]=a["Months"].map(extract)
+print(a)
